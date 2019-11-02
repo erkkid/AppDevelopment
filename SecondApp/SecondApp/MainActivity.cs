c3476b7239f4cd3768c435a55bc9dbfe6fa00181
Erkkid <erkki.dreiak@tptlive.ee>
2019-11-02 12:48:45 +0200

    Second commit

---
+++ SecondApp/SecondApp/MainActivity.cs
@@ -0,0 +1,40 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+using Android.Content;
+using Android.Views;
+using Android.Webkit;
+
+namespace SecondApp
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+
+            var toSecondActivityButton = FindViewById<Button>(Resource.Id.button1);
+            var toWebviewActivityButton = FindViewById<Button>(Resource.Id.button2);
+            var editText = FindViewById<EditText>(Resource.Id.editText1);
+
+            toSecondActivityButton.Click += delegate
+            {
+                var text = editText.Text;
+                var intent = new Intent(this, typeof(SecondActivity));
+                intent.PutExtra("editextvalue", text);
+                StartActivity(intent);
+            };
+
+            toWebviewActivityButton.Click += delegate
+            {
+                var intent = new Intent(this, typeof(WebView));
+                StartActivity(intent);
+            };
+        }
+    }
+}
