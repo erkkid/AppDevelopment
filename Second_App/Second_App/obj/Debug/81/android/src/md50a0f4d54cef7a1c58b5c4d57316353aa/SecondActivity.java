de0d46a24c97bbdce4194fff191051c5a641235e
Kahuka <48998792+Kahuka@users.noreply.github.com>
2019-11-02 10:36:55 +0200

    First commit

---
+++ Second_App/Second_App/obj/Debug/81/android/src/md50a0f4d54cef7a1c58b5c4d57316353aa/SecondActivity.java
@@ -0,0 +1,47 @@
+package md50a0f4d54cef7a1c58b5c4d57316353aa;
+
+
+public class SecondActivity
+	extends android.app.Activity
+	implements
+		mono.android.IGCUserPeer
+{
+/** @hide */
+	public static final String __md_methods;
+	static {
+		__md_methods = 
+			"n_onCreate:(Landroid/os/Bundle;)V:GetOnCreate_Landroid_os_Bundle_Handler\n" +
+			"";
+		mono.android.Runtime.register ("Second_App.Resources.SecondActivity, Second_App", SecondActivity.class, __md_methods);
+	}
+
+
+	public SecondActivity ()
+	{
+		super ();
+		if (getClass () == SecondActivity.class)
+			mono.android.TypeManager.Activate ("Second_App.Resources.SecondActivity, Second_App", "", this, new java.lang.Object[] {  });
+	}
+
+
+	public void onCreate (android.os.Bundle p0)
+	{
+		n_onCreate (p0);
+	}
+
+	private native void n_onCreate (android.os.Bundle p0);
+
+	private java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
